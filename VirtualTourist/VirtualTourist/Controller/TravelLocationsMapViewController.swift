9e042d9ef7bb5fd3c0f18eb83687b9904138a3fb
GiovanniLuigi <giovannibruno128@gmail.com>
2021-01-01 12:28:52 -0300

    saving current region on map

---
+++ VirtualTourist/VirtualTourist/Controller/TravelLocationsMapViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Giovanni Luidi Bruno on 31/12/20.
+//  Copyright © 2020 Giovanni Luigi Bruno. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var dataManager: DataManager {
+        return DataManager.shared
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        
+        if let region: CoordinateRegion = dataManager.get() {
+            mapView.setRegion(region.toMKCoordinateRegion(), animated: false)
+        }
+    }
+    
+    
+}
+
+
+extension TravelLocationsMapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let region = mapView.region.toCoordinateRegion()
+        dataManager.save(object: region)
+    }
+}
+
+extension MKCoordinateRegion {
+    func toCoordinateRegion() -> CoordinateRegion {
+        return CoordinateRegion(centerLatitude: center.latitude, latitudeDelta: span.latitudeDelta, centerLongitude: center.longitude, longitudeDelta: span.longitudeDelta)
+    }
+}
+
+struct CoordinateRegion: Codable {
+    let centerLatitude: Double
+    let latitudeDelta: Double
+    let centerLongitude: Double
+    let longitudeDelta: Double
+    
+    func toMKCoordinateRegion() -> MKCoordinateRegion {
+        return MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude), span: MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta))
+    }
+}
---
+++ VirtualTourist/VirtualTourist/Model/DataManager.swift
@@ -0,0 +1,68 @@
+//
+//  DataMAnager.swift
+//  VirtualTourist
+//
+//  Created by Giovanni Luidi Bruno on 01/01/21.
+//  Copyright © 2021 Giovanni Luigi Bruno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class DataManager {
+    static let shared = DataManager()
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "VirtualTourist")
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { [weak self] (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self?.autosave()
+            completion?()
+            print("loaded")
+        }
+    }
+    
+    func autosave() {
+        saveContext()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) { [weak self] in
+            self?.autosave()
+        }
+    }
+    
+    func saveContext() {
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+    }
+}
+
+// Handles user preferences
+extension DataManager {
+    
+    func save<T: Codable>(object: T) {
+        print(String(describing: T.self))
+        if let data = try? JSONEncoder().encode(object) {
+            UserDefaults.standard.set(data, forKey: String(describing: T.self))
+        }
+    }
+    
+    func get<T: Codable>() -> T? {
+        if let data = UserDefaults.standard.value(forKey: String(describing: T.self)) as? Data ,let object = try? JSONDecoder().decode(T.self, from: data) {
+            return object
+        }
+        return nil
+    }
+    
+}
